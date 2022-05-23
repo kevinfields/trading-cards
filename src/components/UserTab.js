73d1bef30803068e2c8e5602a8aaf4f52448b1b2
kevinfields <94638183+kevinfields@users.noreply.github.com>
2022-05-23 16:11:02 -0500

    minor user tab css fix

--- src/components/UserTab.js
+++ src/components/UserTab.js
@@ -4,6 +4,7 @@ import "../styling/UserTab.css";
 const UserTab = (props) => {
   return (
     <div className="user-tab">
+      <p className="tab-id">{props.user.id}</p>
       <p className="tab-stat">{props.user.name}</p>
       <p className="tab-stat">Total XP: {props.user.xpTotal}</p>
       <p className="tab-stat">Number of Cards: {props.user.cards.length}</p>
