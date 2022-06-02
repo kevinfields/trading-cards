b9d2e2087f4b1a636cb60339d38324513131646d
Kevin Fields <kfields31@gmail.com>
2022-06-02 17:21:28 -0500

    in case i forget

--- src/pages/AlertsPage.js
+++ src/pages/AlertsPage.js
@@ -4,7 +4,6 @@ import Alert from '../components/Alert';
 import LoadingScreen from '../components/LoadingScreen';
 import formatTime from '../functions/formatTime';
 
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 const AlertsPage = (props) => {
 
